9d36755ae2e6a93fe444c1e7da440b4cfcaa2136
luna <lunashoval@gmail.com>
2021-10-30 17:39:49 -0400

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const display = document.querySelector('.display')
+//all the number button section
 const num0 = document.querySelector('.num0');
 const num1 = document.querySelector('.num1');
 const num2 = document.querySelector('.num2');
@@ -9,80 +10,151 @@ const num6 = document.querySelector('.num6');
 const num7 = document.querySelector('.num7');
 const num8 = document.querySelector('.num8');
 const num9 = document.querySelector('.num9');
+let a;
+let b;
 
 let displayValue = '';
 num0.addEventListener('click', () => {
     displayValue = '0';
     display.textContent += displayValue;
+    if(a === undefined){
+        a = 0;
+    }
+    if(b === undefined){
+        b = 0;
+    }
 });
 num1.addEventListener('click', () => {
     displayValue = '1';
     display.textContent += displayValue;
+    if(a === undefined){
+        a = 1;
+    }
+    if(b === undefined){
+        b = 1;
+    }
 });
 num2.addEventListener('click', () => {
     displayValue = '2';
     display.textContent += displayValue;
+    if(a === undefined){
+        a = 2;
+    }
+    if(b === undefined){
+        b = 2;
+    }
 });
 num3.addEventListener('click', () => {
     displayValue = '3';
     display.textContent += displayValue;
+    if(a === undefined){
+        a = 3;
+    }
+    if(b === undefined){
+        b = 3;
+    }
 });
 num4.addEventListener('click', () => {
     displayValue = '4';
     display.textContent += displayValue;
+    if(a === undefined){
+        a = 4;
+    }
+    if(b === undefined){
+        b = 4;
+    }
 });
 num5.addEventListener('click', () => {
     displayValue = '5';
     display.textContent += displayValue;
+    if(a === undefined){
+        a = 5;
+    }
+    if(b === undefined){
+        b = 5;
+    }
 });
 num6.addEventListener('click', () => {
     displayValue = '6';
     display.textContent += displayValue;
+    if(a === undefined){
+        a = 6;
+    }
+    if(b === undefined){
+        b = 6;
+    }
 });
 num7.addEventListener('click', () => {
     displayValue = '7';
     display.textContent += displayValue;
+    if(a === undefined){
+        a = 7;
+    }
+    if(b === undefined){
+        b = 7;
+    }
 });
 num8.addEventListener('click', () => {
     displayValue = '8';
     display.textContent += displayValue;
+    if(a === undefined){
+        a = 8;
+    }
+    if(b === undefined){
+        b = 8;
+    }
 });
 num9.addEventListener('click', () => {
     displayValue = '9';
     display.textContent += displayValue;
+    if(a === undefined){
+        a = 9;
+    }
+    if(b === undefined){
+        b = 9;
+    }
 });
+//all the operator buttons section
 const equalBtn = document.querySelector('.opEq');
 const addBtn = document.querySelector('.opPl');
 const subtractBtn = document.querySelector('.opSu');
 const multiplyBtn = document.querySelector('.opMu');
 const divideBtn = document.querySelector('.opDi');
 const clearBtn = document.querySelector('.opC');
+let whichOp;
 
-equalBtn.addEventListener('click', () => {
-    displayValue = '=';
+equalBtn.addEventListener('click', (a,b) => {
+    displayValue = operate(a,b);
     display.textContent += displayValue;
 });
 addBtn.addEventListener('click', () => {
     displayValue = '+';
     display.textContent += displayValue;
+    whichOp = add(a,b);
 });
 subtractBtn.addEventListener('click', () => {
     displayValue = '-';
     display.textContent += displayValue;
+    whichOp = subtract(a,b);
 });
 multiplyBtn.addEventListener('click', () => {
     displayValue = '*';
     display.textContent += displayValue;
+    whichOp = multiply(a,b);
 });
 divideBtn.addEventListener('click', () => {
     displayValue = '/';
     display.textContent += displayValue;
+    whichOp = divide(a,b);
 });
 clearBtn.addEventListener('click', () => {
     displayValue = '';
     display.textContent = displayValue;
+    a = undefined;
+    b = undefined;
+    result = undefined;
 });
-
+//calculator math section
 const add = function(a,b){
     let result = a + b;
         return result
@@ -100,6 +172,6 @@ const divide = function(a,b){
         return result
 }
 function operate(a,b){
-    let finalResult = multiply(a,b)
+    let finalResult = whichOp;
         return finalResult
 }
