88b21a8dab694160cc011791c6a77961c5a9d860
luna <lunashoval@gmail.com>
2021-10-30 10:08:43 -0400

    script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const add = function(a,b){
+    let result = a + b;
+        return result
+}
+const subtract = function(a,b){
+    let result = a - b;
+        return result
+}
+const multiply = function(a,b){
+    let result = a * b;
+        return result
+}
+const divide = function(a,b){
+    let result = a / b;
+        return result
+}
+function operate(a,b){
+    let finalResult = multiply(a,b)
+        return finalResult
+}
+console.log(operate(2,3))
