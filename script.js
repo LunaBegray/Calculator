0050a75d5871557fa1bb16b38a5824d9d1fd8f38
luna <lunashoval@gmail.com>
2021-10-30 11:35:44 -0400

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
+const display = document.querySelector('.display')
+const num0 = document.querySelector('.num0');
+const num1 = document.querySelector('.num1');
+const num2 = document.querySelector('.num2');
+const num3 = document.querySelector('.num3');
+const num4 = document.querySelector('.num4');
+const num5 = document.querySelector('.num5');
+const num6 = document.querySelector('.num6');
+const num7 = document.querySelector('.num7');
+const num8 = document.querySelector('.num8');
+const num9 = document.querySelector('.num9');
+let displayValue;
+num0.addEventListner('click', () => {
+    displayValue = 0;
+    display.textContent = displayValue;
+});
+num1.addEventListner('click', () => {
+    displayValue = 1;
+    display.textContent = displayValue;
+});
+num2.addEventListner('click', () => {
+    displayValue = 2;
+    display.textContent = displayValue;
+});
+num3.addEventListner('click', () => {
+    displayValue = 3;
+    display.textContent = displayValue;
+});
+num4.addEventListner('click', () => {
+    displayValue = 4;
+    display.textContent = displayValue;
+});
+num5.addEventListner('click', () => {
+    displayValue = 5;
+    display.textContent = displayValue;
+});
+num6.addEventListner('click', () => {
+    displayValue = 6;
+    display.textContent = displayValue;
+});
+num7.addEventListner('click', () => {
+    displayValue = 7;
+    display.textContent = displayValue;
+});
+num8.addEventListner('click', () => {
+    displayValue = 8;
+    display.textContent = displayValue;
+});
+num9.addEventListner('click', () => {
+    displayValue = 9;
+    display.textContent = displayValue;
+});
 const add = function(a,b){
     let result = a + b;
         return result
@@ -18,4 +70,3 @@ function operate(a,b){
     let finalResult = multiply(a,b)
         return finalResult
 }
-console.log(operate(2,3))
